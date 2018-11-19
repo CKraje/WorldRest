d415075fe70a5e758c7f32eb21c6fdc6885f239f
Ivan Maltese <ivan.maltese@objectmethod.it>
2018-11-19 12:19:34 +0100

    bootstrap improvements and bugfix

--- World/src/main/webapp/home.jsp
+++ World/src/main/webapp/home.jsp
@@ -1,58 +1,79 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Insert title here</title>
 <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+<script src="/bootstrap/js/bootstrap.min.js"></script>
 <!--  <script src="/bootstrap/js/bootstrap.min.js"></script>-->
 <link rel="stylesheet" type="text/css" href="/css/style.css">
 <script type="text/javascript" src="/js/countries-continent.js"></script>
 <script type="text/javascript" src="/js/city.js"></script>
 <script type="text/javascript" src="/js/validator.js"></script>
 </head>
-<body >
-	<input type="text" id="cityName"
-		placeholder="inserci nome City da cercare" class="form-control"
-		style="width: 250px;">
-		<input type="button" onclick="searchCities()" class="btn btn-secondary" value="Cerca Citta"
-		id="searchCityButton" >
-		<input type="button" class="btn btn-secondary" value="Crea Citta" onclick="formToCreate()">
-	<form id="formToInsertUpdate" style="display: none;" >
-		<table>
-			<tbody>
-				<tr>
-					<td><input type="text" id="cityNameOfForm"
-						class="form-control" style="width: 250px;"
-						placeholder="inserci nome City"></td>
-					<td><select id="countriesList" class="custom-select"
-						style="width: 250px;">
-					</select></td>
-					<input type="hidden" id="cityId" value="0" >
-				</tr>
-				<tr>
-					<td><input type="text" id="cityDistrictOfForm"
-						style="width: 250px;" placeholder="distretto citta"
-						class="form-control"></td>
-					<td><input type="text" style="width: 250px;"
-						id="cityPopulationOfForm" placeholder="popolazione citta"
-						class="form-control"></td>
-						<td><button id="modificaCityButton"class="btn btn-secondary"
-						onclick="saveCity()"></button></td>
-						<td> <p id="updateMessage"style="display: none">Modifica Avvenuta con Successo !</p></td>
-				</tr>
-			</tbody>
-		</table>
-	</form>
-	<div align="center" id="main-content"></div>
-	<div align="center">
-		<input type="button" style="display: none" class="btn btn-secondary"
-			value="Torna ai continenti" onclick="getContinents()"
-			id="buttonContinenti">
+<body>
+
+	<div class="container">
+
+		<div class="row">
+			<div class="col-md-4 offset-md-4 mt-1">
+				<input type="text" id="cityName"
+					placeholder="Inserisci nome della città" class="form-control">
+			</div>
+			<div class="col-md-2 col-6 mt-1">
+				<button onclick="searchCities()" class="btn btn-secondary w-100"
+					id="searchCityButton">Cerca Citta</button>
+			</div>
+			<div class="col-md-2 col-6 mt-1">
+				<button class="btn btn-secondary w-100" onclick="formToCreate()">Crea
+					Citta</button>
+			</div>
+
+
+		</div>
+
+
+
+		<form id="formToInsertUpdate" style="display: none;">
+			<input type="hidden" id="cityId" value="0">
+			<table>
+				<tbody>
+					<tr>
+						<td><input type="text" id="cityNameOfForm"
+							class="form-control" style="width: 250px;"
+							placeholder="inserci nome City"></td>
+						<td><select id="countriesList" class="custom-select"
+							style="width: 250px;">
+						</select></td>
+					</tr>
+					<tr>
+						<td><input type="text" id="cityDistrictOfForm"
+							style="width: 250px;" placeholder="distretto citta"
+							class="form-control"></td>
+						<td><input type="text" style="width: 250px;"
+							id="cityPopulationOfForm" placeholder="popolazione citta"
+							class="form-control"></td>
+						<td><button id="modificaCityButton" class="btn btn-secondary">Salva</button></td>
+						<td>
+							<p id="updateMessage" style="display: none">Modifica Avvenuta
+								con Successo !</p>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</form>
+
+		<div align="center" id="main-content"></div>
+
+		<div align="center">
+			<input type="button" style="display: none" class="btn btn-secondary"
+				value="Torna ai continenti" onclick="getContinents()"
+				id="buttonContinenti">
+		</div>
 	</div>
-	
-	<script src="/bootstrap/js/bootstrap.min.js"></script>
 </body>
-<script type="text/javascript" >getContinents()</script>
+<script type="text/javascript">
+	getContinents()
+</script>
 </html>
--- World/src/main/webapp/js/city.js
+++ World/src/main/webapp/js/city.js
@@ -1,11 +1,11 @@
-function getCities(countryCode,continent){
+function getCities(countryCode, continent) {
 	var xmlhttp = new XMLHttpRequest();
-	var url = "/api/cities/"+countryCode+"/find-by-code";
-	xmlhttp.onreadystatechange = function() {
+	var url = "/api/cities/" + countryCode + "/find-by-code";
+	xmlhttp.onreadystatechange = function () {
 		if (this.readyState == 4 && this.status == 200) {
 			var cities = JSON.parse(this.responseText);
 			console.log("ARRIVATA RISPOSTA!");
-			displayCities(cities,continent);
+			displayCities(cities, continent);
 		}
 	};
 	xmlhttp.open("GET", url, true);
@@ -13,10 +13,10 @@ function getCities(countryCode,continent){
 	console.log("CHIAMATA INVIATA");
 }
 
-function getCityById(id,sendToFormCity,sendToUpdate){
+function getCityById(id) {
 	var xmlhttp = new XMLHttpRequest();
-	var url = "/api/cities/"+id+"/find-by-id";
-	xmlhttp.onreadystatechange = function() {
+	var url = "/api/cities/" + id + "/find-by-id";
+	xmlhttp.onreadystatechange = function () {
 		if (this.readyState == 4 && this.status == 200) {
 			var city = JSON.parse(this.responseText);
 			console.log("ARRIVATA RISPOSTA!");
@@ -28,9 +28,9 @@ function getCityById(id,sendToFormCity,sendToUpdate){
 	console.log("CHIAMATA INVIATA");
 }
 
-function displayCities(cities,continent){
+function displayCities(cities, continent) {
 	var table = document.createElement('TABLE');
-	table.setAttribute('class','table table-bordered');
+	table.setAttribute('class', 'table table-bordered');
 	table.setAttribute("style", "width: 400px; margin-top: 25px;");
 	var rowThead = table.insertRow();
 	var thCityName = document.createElement('th');
@@ -46,89 +46,91 @@ function displayCities(cities,continent){
 	rowThead.appendChild(thCityPopulation);
 	rowThead.appendChild(thModificaCity);
 	buttonModificaCity
-	var cityCode="";
+	var cityCode = "";
 	var i;
-	for(i = 0; i < cities.length; i++) {
+	for (i = 0; i < cities.length; i++) {
 		row = table.insertRow();
 		var buttonCityName = document.createElement("BUTTON");
 		var buttonCityDistrict = document.createElement("BUTTON");
 		var buttonCityPopulation = document.createElement("BUTTON");
 		var buttonModificaCity = document.createElement("BUTTON");
-		buttonModificaCity.setAttribute('class','btn btn-link')
-		buttonCityName.setAttribute('class','btn btn-link');
-		buttonCityDistrict.setAttribute('class','btn btn-link');
-		buttonCityPopulation.setAttribute('class','btn btn-link');
-		buttonCityName.innerHTML=cities[i].name;
+		buttonModificaCity.setAttribute('class', 'btn btn-link')
+		buttonCityName.setAttribute('class', 'btn btn-link');
+		buttonCityDistrict.setAttribute('class', 'btn btn-link');
+		buttonCityPopulation.setAttribute('class', 'btn btn-link');
+		buttonCityName.innerHTML = cities[i].name;
 		var tdCityName = row.insertCell();
 		tdCityName.appendChild(buttonCityName);
 		var tdCityDistrict = row.insertCell();
-		buttonCityDistrict.innerHTML=cities[i].district;
+		buttonCityDistrict.innerHTML = cities[i].district;
 		tdCityDistrict.appendChild(buttonCityDistrict);
 		var tdCityPopulation = row.insertCell();
-		buttonCityPopulation.innerHTML=cities[i].population;
+		buttonCityPopulation.innerHTML = cities[i].population;
 		tdCityPopulation.appendChild(buttonCityPopulation);
 		var tdModificaCity = row.insertCell();
-		buttonModificaCity.innerHTML="Modifica";
-		buttonModificaCity.setAttribute("onclick","formCity("+cities[i].id+")");
+		buttonModificaCity.innerHTML = "Modifica";
+		buttonModificaCity.setAttribute("onclick", "formCity(" + cities[i].id + ")");
 		tdModificaCity.appendChild(buttonModificaCity);
-		cityCode=cities[i].code;
+		cityCode = cities[i].code;
 	}
 	var content = document.getElementById("main-content");
 	content.innerHTML = '';
 	content.appendChild(table);
-	if(! (continent == null)){
+	if (!(continent == null)) {
 		var buttonBack = document.createElement('BUTTON');
-		buttonBack.setAttribute("onclick","getCountries('"+continent+"')");
+		buttonBack.setAttribute("onclick", "getCountries('" + continent + "')");
 		buttonBack.innerHTML = 'Back';
-		buttonBack.setAttribute("id","backToCountries");
-		buttonBack.setAttribute("class","btn btn-secondary");
-		buttonBack.setAttribute("id","backToCountries");
-		setVisibility(true,true, false);
+		buttonBack.setAttribute("id", "backToCountries");
+		buttonBack.setAttribute("class", "btn btn-secondary");
+		buttonBack.setAttribute("id", "backToCountries");
+		setVisibility(true, true, false);
 		content.appendChild(buttonBack);
 	}
-	else{
-		setVisibility(false,true, true);
+	else {
+		setVisibility(false, true, true);
 	}
 	getCountriesToForm(cityCode);
 }
 
-function fillCityForm(city){
-	document.getElementById("cityId").value=city.id;
+function fillCityForm(city) {
+	document.getElementById("cityId").value = city.id;
 	document.getElementById("cityNameOfForm").value = city.name;
-	document.getElementById("cityDistrictOfForm").value=city.district;
-	document.getElementById("cityPopulationOfForm").value=city.population;
-	var modifyCityButton = document.getElementById("modificaCityButton");
-	modifyCityButton.innerHTML ="modifica";
+	document.getElementById("cityDistrictOfForm").value = city.district;
+	document.getElementById("cityPopulationOfForm").value = city.population;
 }
 
-function formCity(idCity){
-	setVisibility(true,false,false);
-	getCityById(idCity,true,false);
+function formCity(idCity) {
+	setVisibility(true, false, false);
+	getCityById(idCity, true, false);
+	document.getElementById("modificaCityButton").addEventListener("click", function(event) {
+		event.preventDefault();
+		saveCity();
+	});
 }
 
 
-function searchCities(){
+function searchCities() {
 	var citynameTextBox = document.getElementById("cityName");
 	var cityName = citynameTextBox.value;
 	var xmlhttp = new XMLHttpRequest();
 	var url = "/api/cities/search";
-	xmlhttp.onreadystatechange = function() {
+	xmlhttp.onreadystatechange = function () {
 		if (this.readyState == 4 && this.status == 200) {
 			var citiesByName = JSON.parse(this.responseText);
-			displayCities(citiesByName,null);
+			displayCities(citiesByName, null);
 			console.log("ARRIVATA RISPOSTA!");
-			setVisibility(false,true,false);  
+			setVisibility(false, true, false);
 		}
 	};
 	xmlhttp.open("POST", url, true);
 	xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	xmlhttp.send("param="+cityName); 
+	xmlhttp.send("param=" + cityName);
 	console.log("CHIAMATA INVIATA");
 }
 
-function saveCity(){
-	var city ={};
-	city.id= document.getElementById("cityId").value;
+function saveCity() {
+	var city = {};
+	city.id = document.getElementById("cityId").value;
 	city.name = document.getElementById("cityNameOfForm").value;
 	city.district = document.getElementById("cityDistrictOfForm").value;
 	city.population = document.getElementById("cityPopulationOfForm").value;
@@ -137,33 +139,31 @@ function saveCity(){
 	console.log(city);
 	var xmlhttp = new XMLHttpRequest();
 	var url = "/api/cities/insert_modify";
-	xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200 ) {
-			console.log("ARRIVATA RISPOSTA , UPLOAD!");
+	xmlhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
 			var cityUploaded = JSON.parse(this.responseText);
-			if(city.id != 0){
-			document.getElementById("updateMessage").innerHTML = "Modifica Avvenuta con Successo !"
+			if (city.id != 0) {
+				document.getElementById("updateMessage").innerHTML = "Modifica Avvenuta con Successo !"
 			}
-			document.getElementById("updateMessage").setAttribute("style","display: block;");
+			document.getElementById("updateMessage").setAttribute("style", "display: block;");
 			fillCityForm(cityUploaded);
 		}
 	};
 	xmlhttp.open("POST", url, true);
-	xmlhttp.setRequestHeader("Content-Type","application/json");
+	xmlhttp.setRequestHeader("Content-Type", "application/json");
 	var data = JSON.stringify(city);
 	xmlhttp.send(data);
-	console.log("CHIAMATA INVIATA");
 }
 
-function formToCreate(){
-	 setVisibility(true,false,false);
+function formToCreate() {
+	setVisibility(true, false, false);
 	clearCountriesOptionsSelect();
 	getCountriesToForm("");
-	document.getElementById("cityId").value=0;
+	document.getElementById("cityId").value = 0;
 	document.getElementById("cityNameOfForm").value = "";
-	document.getElementById("cityDistrictOfForm").value="";
-	document.getElementById("cityPopulationOfForm").value="";
-	document.getElementById("modificaCityButton").innerHTML ="crea";
+	document.getElementById("cityDistrictOfForm").value = "";
+	document.getElementById("cityPopulationOfForm").value = "";
+	document.getElementById("modificaCityButton").innerHTML = "crea";
 	document.getElementById("updateMessage").innerHTML = "Creazione avvenuta!"
-	document.getElementById("updateMessage").setAttribute("style","display: none;");	
+	document.getElementById("updateMessage").setAttribute("style", "display: none;");
 }
