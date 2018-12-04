f7b701282a1dc3d04dd6b2a0bb6a5ff053d95e58
Ivan Maltese <ivan.maltese@objectmethod.it>
2018-12-04 14:30:04 +0100

    corretto (parzialmente) bug su firefox

--- World/src/main/webapp/js/initializer.js
+++ World/src/main/webapp/js/initializer.js
@@ -1,47 +1,57 @@
-$(document).ready(function() {
-	$("body").css("background","#eee");
+$(document).ready(function () {
+	$("body").css("background", "#eee");
 	var url = "/api/continents";
-	$.get(url, function(continents) {
+	$.get(url, function (continents) {
 		displayContinents(continents);
 	});
-	$("#searchButton").click(function() {
-		event.preventDefault();
+	$("#searchButton").click(function (event) {
+		ie8SafePreventEvent(event);
 		searchCities();
 	});
-	$("#create-city-button").on("click",function() {
+	$("#create-city-button").on("click", function () {
 		event.preventDefault();
 		formToCreate();
-		$("#create-city-button").css('display','none');
-		$("#close-form").css('display','block');
-		$("#modificaCityButton").css('display','block');
+		$("#create-city-button").css('display', 'none');
+		$("#close-form").css('display', 'block');
+		$("#modificaCityButton").css('display', 'block');
 		$("#formToInsertUpdate").show();
 	});
-	$("#close-form").on("click",function(){
+	$("#close-form").on("click", function () {
 		event.preventDefault();
-		if ($("#formToInsertUpdate").css('display')==='block') {
+		if ($("#formToInsertUpdate").css('display') === 'block') {
 			$("#formToInsertUpdate").hide();
-			$("#close-form").css('display','none');
-			$("#create-city-button").css('display','block');
+			$("#close-form").css('display', 'none');
+			$("#create-city-button").css('display', 'block');
 		}
-		else if ($("#formToInsertUpdate").css('display')==='none' || $("#cityId").val()!= 0){
+		else if ($("#formToInsertUpdate").css('display') === 'none' || $("#cityId").val() != 0) {
 			$("#formToInsertUpdate").show();
-			$("#close-form").css('display','block');
-			$("#create-city-button").css('display','none');
+			$("#close-form").css('display', 'block');
+			$("#create-city-button").css('display', 'none');
 		}
 	})
 });
 
 
-function scrollToForm(){
+function scrollToForm() {
 	$('html, body').stop().animate({
 		scrollTop: $("#formToInsertUpdate").offset().top
 	}, 500);
 }
 
-function closeForm(){
-	$("#formToInsertUpdate").css('display','none');
-	$("#close-save-button").css('display','none');
-	$("#close-form").css('display','none');
-	$("#create-city-button").css('display','block');
-	$("#modificaCityButton").css('display','block');
+function closeForm() {
+	$("#formToInsertUpdate").css('display', 'none');
+	$("#close-save-button").css('display', 'none');
+	$("#close-form").css('display', 'none');
+	$("#create-city-button").css('display', 'block');
+	$("#modificaCityButton").css('display', 'block');
+}
+
+function ie8SafePreventEvent(e) {
+	if (e.preventDefault) { 
+		e.preventDefault() 
+	} else { 
+		e.stop() 
+	};
+	e.returnValue = false;
+	e.stopPropagation();
 }
